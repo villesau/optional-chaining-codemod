500fa7259ab6fe150afa4444014d22202bbba9c5
Ville Saukkonen <ville@smartly.io>
2018-10-29 22:36:03 +0200

    fix transform script path

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 const jscodeshiftExecutable = require.resolve('.bin/jscodeshift');
+const path = require('path');
 const execa = require('execa');
 process.argv.shift();
 process.argv.shift();
-const args = ['-t', './transform.js', ...process.argv];
+const args = ['-t', path.resolve(__dirname, '../transform.js'), ...process.argv];
 const result = execa.sync(jscodeshiftExecutable, args, {
   stdio: 'inherit',
   stripEof: false,
