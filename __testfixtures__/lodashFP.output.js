4aae40ab0df701527474f84bcf3fe281c5bba5a8
Jorge Gonzalez <jorgegonzalez@users.noreply.github.com>
2020-01-09 20:23:20 -0500

    Swap arguments

--- __testfixtures__/lodashFP.output.js
+++ __testfixtures__/lodashFP.output.js
@@ -1,13 +1,13 @@
 // @flow
-const foo1 = bar?.a?.b?.c;
-const foo2 = bar?.a?.b?.c;
-const foo3 = bar?.a?.b?.c;
-const foo4 = bar?.a?.[2]?.c;
-const foo5 = bar?.a?.[foo5]?.c;
-const foo6 = bar?.a?.[321]?.c;
-const foo7 = bar?.a?.[this.smthng]?.c;
-const foo8 = bar?.a?.[foo5]?.c ?? 123;
-const foo9 = bar?.a?.[foo5]?.c ?? "what";
+const foo1  = bar?.a?.b?.c;
+const foo2  = bar?.a?.b?.c;
+const foo3  = bar?.a?.b?.c;
+const foo4  = bar?.a?.[2]?.c;
+const foo5  = bar?.a?.[foo5]?.c;
+const foo6  = bar?.a?.[321]?.c;
+const foo7  = bar?.a?.[this.smthng]?.c;
+const foo8  = bar?.a?.[foo5]?.c ?? 123;
+const foo9  = bar?.a?.[foo5]?.c ?? "what";
 const foo10 = bar?.a?.[foo5]?.c ?? barr;
 const foo11 = bar?.a?.[foo5];
 const foo12 = bar?.a?.[foo5]?.smthng;
